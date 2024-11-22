e090132092c834ac19981d37cddfeef916d44cc1
YAAKOV BRILL <yaakovbrill@gmail.com>
2024-11-22 19:10:30 +0200

    Removed summary section

--- app/page.tsx
+++ app/page.tsx
@@ -4,13 +4,11 @@ import EducationSection from "./components/sections/educationSection";
 import ExperienceSection from "./components/sections/experienceSection";
 import ProjectsSection from "./components/sections/projectsSection";
 import SkillsSection from "./components/sections/skillsSection";
-import SummarySection from "./components/sections/summarySection";
 
 export default function Home() {
   return (
     <div className="max-w-4xl mx-auto px-6 mb-8">
       <ContactSection/>
-      <SummarySection/>
       <SkillsSection/>
       <ExperienceSection/>
       <EducationSection/>
